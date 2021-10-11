237e7f6e0c7c38826cd2a7f258b584efd3729e52
Ming-beginner <87808879+Ming-beginner@users.noreply.github.com>
2021-10-11 23:06:13 +0700

    deploy

---
+++ homework/index.js
@@ -0,0 +1,17 @@
+/* Bài 1 */
+
+let surName = "Le";
+let firstName = "Ngoc";
+let lastName = "Minh";
+/* ==========Bài 2========== */
+alert("Bài 1: " + surName + " " + firstName + " " + lastName)
+console.log("Bài 1: " + surName + " " + firstName + " " + lastName)
+
+alert("Bài 2: " + "7 chia 2 dư: " + String(7%2) )
+console.log("Bài 2: 7 chia 2 dư: " + String(7%2) )
+
+/* ==========Bài 3========== */
+let text = "Hello World"
+alert("Bài 3: Độ sài của \"Hello World\" là: " + String(text.length))
+console.log("Bài 3: Độ sài của \"Hello World\" là: " + String(text.length))
+
